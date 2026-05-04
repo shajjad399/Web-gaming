eb497c145167ee22e89c0198e3b00ffbeec7f5e9
SHAJJAD PROGRAMING <shajjadm3199@gmail.com>
2026-05-04 21:16:34 +0600

    Add files via upload

---
+++ Web gaming/script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach((button) => {
+  button.addEventListener('mouseenter', () => {
+    button.style.transform = 'translateY(-2px)';
+  });
+  button.addEventListener('mouseleave', () => {
+    button.style.transform = '';
+  });
+});
